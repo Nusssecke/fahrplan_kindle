12b0d7ff69e539a92f1db688ac92cb24414754bb
Nusssecke <85316455+Nusssecke@users.noreply.github.com>
2023-03-06 22:01:42 +0100

    Update only_python.sh

--- bin/only_python.sh
+++ bin/only_python.sh
@@ -1,2 +1,2 @@
-sshpass -p "minecraft" scp -r /home/finn/Programmieren/Kindle/fahrplan_kindle root@192.168.15.244:/mnt/base-us/extensions
-sshpass -p "minecraft" ssh root@192.168.15.244 "python3 /mnt/base-us/extensions/fahrplan_kindle/bin/testing.py"
+sshpass -p "password" scp -r /home/finn/Programmieren/Kindle/fahrplan_kindle root@192.168.15.244:/mnt/base-us/extensions
+sshpass -p "password" ssh root@192.168.15.244 "python3 /mnt/base-us/extensions/fahrplan_kindle/bin/testing.py"
