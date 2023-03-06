b9c28b8bccee9619b8035ec8b7467e0c024ccaaf
Nusssecke <85316455+Nusssecke@users.noreply.github.com>
2023-03-06 22:00:12 +0100

    Update kindle_script.sh

--- kindle_script.sh
+++ kindle_script.sh
@@ -1,16 +1,16 @@
 # Connect to the kindle via ssh
 # https://www.mobileread.com/forums/showthread.php?t=204942
 # Toggle USBNetwork to ON, on kindle beforehand
-# Kindle User: root; Password: minecraft
+# Kindle User: root;
 
 # Setup kindle network
 # sudo ifconfig enxee4900000000 192.168.15.201
 # Kindle ssh:
-# sshpass -p "minecraft" ssh root@192.168.15.244
+# sshpass -p password ssh root@192.168.15.244
 
 # Copy files to kindle
-sshpass -p "minecraft" scp -r /home/finn/Programmieren/Kindle/fahrplan_kindle root@192.168.15.244:/mnt/base-us/extensions
+sshpass -p "password" scp -r /home/finn/Programmieren/Kindle/fahrplan_kindle root@192.168.15.244:/mnt/base-us/extensions
 
 # Login to kindle and execute the start_once.sh script
-sshpass -p "minecraft" ssh root@192.168.15.244 "sh /mnt/base-us/extensions/fahrplan_kindle/bin/start_once.sh"
+sshpass - p "password" -ssh root@192.168.15.244 "sh /mnt/base-us/extensions/fahrplan_kindle/bin/start_once.sh"
 exit
